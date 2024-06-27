0f2be77349d747ae36102f864ea0671ea7368ce8
Daniil <daniil.89m@mail.ru>
2024-06-27 19:14:06 +0300

    First commit Create ROUTES for navigation

---
+++ app/src/main/java/com/example/savushkin/presentation/navigation/Routes.kt
@@ -0,0 +1,16 @@
+package com.example.savushkin.presentation.navigation
+
+sealed class Routes(val route : String) {
+
+    data object AuthorizationPage : Routes(route = ROUTE_AUTHORIZATION_PAGE)
+    data object AllRequestsPage : Routes(route = ROUTE_ALL_REQUESTS_PAGE)
+    data object RequestPage : Routes(route = ROUTE_REQUEST_PAGE)
+    data object DirectoryPage : Routes(route = ROUTE_DIRECTORY_PAGE)
+
+    companion object {
+        const val ROUTE_AUTHORIZATION_PAGE = "AuthorizationPage"
+        const val ROUTE_ALL_REQUESTS_PAGE = "AllRequestsPage"
+        const val ROUTE_REQUEST_PAGE = "RequestPage"
+        const val ROUTE_DIRECTORY_PAGE = "DirectoryPage"
+    }
+}
